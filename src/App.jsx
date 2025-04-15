78b0b706fd58fa7a5aba445d499bbfe22af6ef02
marianacarballo <marianacarballofarias@gmail.com>
2025-04-15 19:48:52 -0300

    agregamos router

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from "./components/Navbar";
-import ItemListContainer from "./components/ItemListContainer";
-// import { Button } from "react-bootstrap"; 
+import ItemListContainer from "./components/ItemListContainer"; 
 import NavbarBootstrap from "./components/NavbarBootstrap";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-//import Button from "./examples/Button";
-// import FetchCountry from "./examples/FetchCountry";
-// //ejemplo de fetch para paises con useEffect y useState 
-// import FetchData from "./examples/FetchData";
-// //ejemplo de fetch para pokemones y dragon ball con map y useEffect
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 
-// import {withLogging} from "./hocs/withLogging";
-// import Hello from "./examples/Hello";
-// esto de arriba es un ejemplo de HOC (High Order Component) que es una funcion que recibe un componente y devuelve otro componente con funcionalidades adicionales.
-// en este caso el HOC es withLogging que recibe el componente Hello y le agrega un console.log cuando se monta el componente.
-// el HOC se puede usar para agregar funcionalidades a cualquier componente sin modificar el componente original.
 
 function App() {  
-console.log('Hola soy App')
-
-//const HelloConLoging = withLogging(Hello)
- 
   return (
-    <>     
+    <BrowserRouter>     
       <NavbarBootstrap/>
-      <ItemListContainer greeting='Bienvenidos a mi primer App ❤ ' />   
-      {/* <FetchCountry/>  */}
-      {/* <FetchData/>
-      <HelloConLoging/> */}
-      {/* aca trabajaremos con la 2da preentrega */}
-      <ItemDetailContainer/>
-    </>
+      <Routes>
+        <Route path='/' element={ <ItemListContainer greeting='Bienvenidos a mi primer App ❤ ' /> }/>        
+        <Route path='/item' element={<ItemDetailContainer/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
